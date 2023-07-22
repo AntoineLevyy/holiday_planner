aa3190877d808c3062fdb09b0639cd8e5d21845a
Antoine Levy <antoine.levy@performics.com>
2023-07-22 16:58:06 +0100

    maps

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from ics import Calendar, Event
 from datetime import datetime
 import base64
 import re
-
+import pandas as pd
 
 
 st.set_page_config(
@@ -42,12 +42,13 @@ city = city_col.text_input("Where are you going?")
 n_days = n_days_col.text_input("How many days?")
 
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = "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"
 @st.cache
 def open_ai_plan_initial(city, n_days):
     open_ai_response = openai.Completion.create(
     model="text-davinci-003",
-    prompt=f"What should I do on holliday in {city} during {n_days} days. Give me each activity in its on line.",
+    prompt=f"What should I do on holliday in {city} during {n_days} days. Give me each activity in its on line.And add the lattitude and longitude in brackets, just the numbers.",
     max_tokens=250,
     temperature=0,
     top_p = 0.3
@@ -55,13 +56,14 @@ def open_ai_plan_initial(city, n_days):
 
     text_resume = open_ai_response['choices'][0]['text']
     text_resume = text_resume.replace("\n\n","")
+    text_resume = text_resume.replace("° N", "").replace("° W", "").replace("° E", "").replace("° S", "").replace("°", "")
     return text_resume
 
 @st.cache
 def open_ai_plan_edited(city, n_days, last_reco,more,less):
     open_ai_response = openai.Completion.create(
     model="text-davinci-003",
-    prompt=f"Can you edit my  activities for {city} where I am spending {n_days} days. The last time you gave this {last_reco}, but actually I would like a new list with more  {more} and less {less}. Give me each activity in its on line.",
+    prompt=f"What should I do on holliday in {city} during {n_days} days. The last time you gave this {last_reco}. I want more {more} and less {less} please format your answer exactly as you did here {open_ai_plan_initial} including the latitude and longitude numbers.",
     max_tokens=250,
     temperature=0,
     top_p = 0.3
@@ -69,6 +71,7 @@ def open_ai_plan_edited(city, n_days, last_reco,more,less):
 
     recommendations = open_ai_response['choices'][0]['text']
     recommendations = recommendations.replace("\n\n","")
+    recommendations = recommendations.replace("° N", "").replace("° W", "").replace("° E", "").replace("° S", "").replace("°", "")
     return recommendations
 
 @st.cache
@@ -78,25 +81,52 @@ def create_download_link(ics_content, filename):
     return href
 
 
-activities = ["","museums", "sightseeing", "partying", "restaurants", "shopping"]
+activity_types = ["","museums", "sightseeing", "partying", "restaurants", "shopping"]
 
 if city != "" and n_days != "":
   st.button("Generate Plan")
   if st.button:
     st.session_state["reco"] = ""
-    last_reco = open_ai_plan_initial(city,n_days)
+    last_open_ai_response = open_ai_plan_initial(city,n_days)
+    last_reco = re.sub(r'\(.*?\)', '', last_open_ai_response)
+    last_reco = re.sub(r'\(.*$', '', last_reco)
     st.session_state["reco"] = last_reco
     st.write(st.session_state["reco"])
+    st.write(" ")
+    st.write(" ")
 
-    ## Add to calendar##
-      # change reco to list without numbers.
-    recommendations_1 = last_reco.split(". ")
-    recommendations_1 = [re.sub(r'\s\d+$', '', recommendation) for recommendation in recommendations_1 if recommendation]
+    ## View on Maps and Add to calendar##
     st.write(" ")
     st.write(" ")
     st.markdown("<div style ='text-align: center;'>Add a recommended activity to your calendar</div>",unsafe_allow_html=True)
+    ###View on maps##
+    #creating a dataframe for maps
+    dict_coordinates = {}
+    last_reco_full = last_open_ai_response.strip().split('\n')
+ 
+    for reco in last_reco_full:
+      if reco:
+          activity, coordinates = re.findall(r"(.*?)(\(.*|$)", reco)[0]
+          # remove leading/trailing whitespaces and digits from the description
+          key = re.sub(r'\d+\. ', '', activity).strip()
+          # remove brackets from coordinates
+          value = coordinates.strip("()")
 
-    selected_recommendation = st.selectbox("Select an activity",recommendations_1[1:])
+          dict_coordinates[key] = value
+   
+    #Getting the list of recommended activities and creating a selectbox
+    activities = list(dict_coordinates.keys())
+    #Select the activity desired
+    selected_recommendation = st.selectbox("Select an activity",activities)
+
+    #Output the selected activity on maps
+    maps_data = [{"Activity": key, "latitude": float(value.split(", ")[0]), "longitude": float(value.split(", ")[1])} 
+    for key, value in dict_coordinates.items()]
+    maps_df_all = pd.DataFrame(maps_data)  
+    maps_df_selected = pd.DataFrame(maps_df_all[maps_df_all["Activity"]==selected_recommendation])
+    st.map(maps_df_selected)
+
+    ###Add to calendar###      
     selected_date = st.date_input("Select a date for the activity")
     # Create a new .ics calendar events
     if st.button("Add to calendar"):
@@ -115,28 +145,55 @@ if city != "" and n_days != "":
     st.markdown("<div style='text-align: center;'>Make some changes to the recommendations</div>", unsafe_allow_html=True)
     more_col, less_col = st.columns(2)
     more_col.write(" ") #this allows for the selectbox to show below the col placeholder
-    more = more_col.selectbox("More of", activities)
+    more = more_col.selectbox("More of", activity_types)
     less_col.write(" ") #this allows for the selectbox to show below the col placeholder
-    less = less_col.selectbox("Less of", activities)
+    less = less_col.selectbox("Less of", activity_types)
     if more != "" and less != "":
         st.button("Edit Plan")
-        new_reco = open_ai_plan_edited(city, n_days, last_reco, more, less)
+        new_reco_full = open_ai_plan_edited(city, n_days, last_reco, more, less)
+        new_reco = re.sub(r'\(.*?\)', '', new_reco_full)
+        new_reco = re.sub(r'\(.*$', '', new_reco)
         st.session_state["reco"] = new_reco
         st.write(st.session_state["reco"])
-         ## Add to calendar##
-         # change reco to list without numbers.
-        recommendations_2 = new_reco.split(". ")
-        recommendations_2 = [re.sub(r'\s\d+$', '', recommendation) for recommendation in recommendations_2 if recommendation]
-        st.write(recommendations_2[2])
-        st.markdown("<div style ='text-align: center;'>Add a recommended activity to your calendar</div>",unsafe_allow_html=True)
-        selected_recommendation_2 = st.selectbox("Select an activity",recommendations_2[1:])
-        selected_date_two = st.date_input("Select a date for the new activity")
+        st.write(" ")
+        st.write(" ")
+
+         ## View on maps and Add to calendar##
+         #creating a dataframe for maps
+        new_dict_coordinates = {}
+        new_reco_full = new_reco_full.strip().split('\n')
+
+        for reco in new_reco_full:
+          if reco:
+              activity, coordinates = re.findall(r"(.*?)(\(.*|$)", reco)[0]
+              # remove leading/trailing whitespaces and digits from the description
+              key = re.sub(r'\d+\. ', '', activity).strip()
+              # remove brackets from coordinates
+              value = coordinates.strip("()")
+
+              new_dict_coordinates[key] = value
+        
+        #Getting the list of recommended activities and creating a selectbox
+        new_activities = list(new_dict_coordinates.keys())
+        #Select the activity desired
+        new_selected_recommendation = st.selectbox("Select a new activity",new_activities)
+
+        #Output the selected activity on maps
+        new_maps_data = [{"Activity": key, "latitude": float(value.split(", ")[0]), "longitude": float(value.split(", ")[1])} 
+        for key, value in new_dict_coordinates.items()]
+        new_maps_df_all = pd.DataFrame(new_maps_data)  
+        new_maps_df_selected = pd.DataFrame(new_maps_df_all[new_maps_df_all["Activity"]==new_selected_recommendation])
+        st.map(new_maps_df_selected) 
+
+
+        ##Add to calendar###      
+        new_selected_date = st.date_input("Select a date for the new activity")
         # Create a new .ics calendar event
         if st.button("Add new activity to calendar"):
             c = Calendar()
             e = Event()
-            e.name = selected_recommendation_2
-            e.begin = datetime.combine(selected_date_two, datetime.min.time()) # use selected date with minimum time
+            e.name = new_selected_recommendation
+            e.begin = datetime.combine(new_selected_date, datetime.min.time()) # use selected date with minimum time
             c.events.add(e)
 
             st.markdown(create_download_link(str(c), "recommendation.ics"), unsafe_allow_html=True)
